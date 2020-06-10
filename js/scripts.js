0d24759ba62d0f229873197c1a4e682379aac376
Ian Scott <chesnekov@gmail.com>
2020-06-10 11:39:19 -0700

    program fulfils first specification, do nothing to non-alphabetical characters
    Co-authored-by: Kelly Eidsvik <kelly.eidsvik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
+// Business Logic
+function pigLatinConverter(string) {
+  return string;
+}
+
+
+// UI Logic
 $(document).ready (function () {
   $("form#piggy-form").submit(function (event) {
     event.preventDefault();
     const userInput = $("input#human-input").val();
 
+    const results = pigLatinConverter(userInput);
+
+    $("#results").text(results);
+    
   });
 });
